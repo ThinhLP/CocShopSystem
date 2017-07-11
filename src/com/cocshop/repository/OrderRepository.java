41213fcbc90f902cc1d9cde0484402754d800e4a
JokerSE <chinhncse62121@gmail.com>
2017-07-11 18:46:47 +0700

    Refactor get order by customer id [ChinhNC]

--- src/com/cocshop/repository/OrderRepository.java
+++ src/com/cocshop/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package com.cocshop.repository;
 
 import com.cocshop.model.TblOrder;
+import com.cocshop.model.TblOrderdetails;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -18,5 +19,12 @@ public interface OrderRepository extends CrudRepository<TblOrder, Integer> {
     @Query(value = "select p from TblOrder  p")
     public List<TblOrder> getAll();
 
+//    @Query(value = "select p from TblOrderdetails  p where p.tblOrderByTblOrderOrderId.tblUserByCustomerId.userId=:userId")
+//    public List<TblOrderdetails> listOrderByCusId(@Param("userId") int userId);
+    @Query(value ="select p from TblOrder p, TblOrderdetails a where p.tblUserByCustomerId.userId=:userId and p.orderId=a.tblOrderByTblOrderOrderId.orderId")
+    public List<TblOrder> listOrderByCustomerId(@Param("userId") int userId);
 
+    // Edit by ThinhLP
+    @Query(value ="select o from TblOrder o where o.tblUserByCustomerId.userId=:userId AND o.tblUserByEmployeeId is not null")
+    public List<TblOrder> listOrderByCustomer(@Param("userId") int userId);
 }
